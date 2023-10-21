94ac618ee646c4b407b3398f27b947d219a3a148
TWintersww <134812478+TWintersww@users.noreply.github.com>
2023-10-20 23:25:44 -0700

    Finish courseinfo

--- Part1Practice/part1/src/App.jsx
+++ Part1Practice/part1/src/App.jsx
@@ -1,11 +1,55 @@
-const App = () => {
-  const friends = [ 'Peter', 'Maya']
+import { useState } from 'react'
 
+const History = (props) => {
+  if (props.clickArr.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
     <div>
-      <p>{friends}</p>
+      button press history: {props.clickArr.join(' ')}
     </div>
   )
 }
+const Button = (props) => {
+  return (
+    <button onClick={props.handlerFunction}>
+      {props.text}
+    </button>
+  )
+}
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [clickArr, setArr] = useState([])
+  const [totalClicks, setTotal] = useState(0)
+
+  const updateLeft = () => {
+    setArr(clickArr.concat('L'));
+    const newLeft = left + 1;
+    setLeft(newLeft);
+    setTotal(newLeft + right);
+  }
+  const updateRight = () => {
+    setArr(clickArr.concat('R'));
+    const newRight = right + 1;
+    setRight(newRight);
+    setTotal(left + newRight);
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handlerFunction={updateLeft} text="left" />
+      <Button handlerFunction={updateRight} text="right" />
+      {right}
+      <History clickArr={clickArr} />
+    </div>
+  )
+}
+
 
 export default App
