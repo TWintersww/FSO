7e6de66de6e7e10c3afd2abdb03dbdd955f68c02
TWintersww <134812478+TWintersww@users.noreply.github.com>
2023-10-26 03:19:22 -0700

    Finish 3.14

--- TryFly/Part3Practice/index.js
+++ TryFly/Part3Practice/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -41,58 +43,40 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello world!</h1>')
 })
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(n => n.id === id)
-    
-    //render note's json or return 404 if note with specified id DNE
-    if (note) {
-        return response.json(note)
-    }
-    else {
-        return response.status(404).end()
-    }
-})
-app.delete('/api/notes/:id', (request, response) => {
-
-    const id = Number(request.params.id)
-    notes = notes.filter(n => n.id !== id)
-
-    
-    response.status(204).end()
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0 
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
+
 app.post('/api/notes', (request, response) => {
     const body = request.body
-
-    if (!body) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+  
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
-
-    const newObject = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId()
-    }
-
-    notes = notes.concat(newObject)
-
-    response.json(newObject)
-})
+  
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+    })
+  
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
+  })
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+
+
